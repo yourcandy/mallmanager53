6e488f570590b9d1ec436f7cb9f8d8327b5ca10a
yourcandy <545082571@qq.com>
2020-04-05 17:51:14 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
