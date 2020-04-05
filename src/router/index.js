7e5eca191e3d8ce40a74602388b4c515146e6212
yourcandy <545082571@qq.com>
2020-04-05 18:14:00 +0800

    新建登录分支 组件路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 Vue.use(Router)
-
+import Login from '@/components/login/login.vue'
 export default new Router({
   routes: [
     {
-      path: '/',
+      name:'login',
+      path: '/login',
+      component:Login
     }
   ]
 })
