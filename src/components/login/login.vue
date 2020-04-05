df4fcd66a991fa4d4e26d264cd35e0ac1dfe0eac
yourcandy <545082571@qq.com>
2020-04-05 19:55:05 +0800

    显示home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -37,7 +37,7 @@
             meta:{msg,status}
           } = res.data
           if (status === 200) {
-            // this.$router.push({name:'home'})
+            this.$router.push({name:'home'});
             this.$message.success(msg)
           }else {
             this.$message.warning(msg)
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 import Login from '@/components/login/login.vue'
+import Home from '@/components/home/home.vue'
 export default new Router({
   routes: [
     {
       name:'login',
       path: '/login',
       component:Login
+    },
+    {
+      name:'home',
+      path: '/',
+      component:Home
     }
   ]
 })
