6977f4d0d968e4675c32d40cad9f6107fec10570
yourcandy <545082571@qq.com>
2020-04-05 19:43:17 +0800

    引入提示框 提示框

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,68 @@
 <template>
-    <div>登录</div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="top"
+      label-width="80px"
+      :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button
+        @click.prevent="handleLogin()"
+        class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "login"
+  export default {
+    data(){
+      return {
+        formdata:{
+          username:'',
+          password:''
+        }
+      }
+    },
+    methods: {
+      handleLogin(){
+        this.$http.post('login',this.formdata).then(res => {
+          const {
+            data,
+            meta:{msg,status}
+          } = res.data
+          if (status === 200) {
+            // this.$router.push({name:'home'})
+            this.$message.success(msg)
+          }else {
+            this.$message.warning(msg)
+          }
+        })
+      }
     }
+  }
 </script>
 
-<style scoped>
-
+<style>
+  .login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .login-wrap .login-form{
+    width: 400px;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 30px;
+  }
+  .login-wrap .login-btn{
+    width: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
+import MyServerHttp from '@/plugins/http.js'
+
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/reset.css';
 import router from './router'
 
+//使用vue插件
+Vue.use(MyServerHttp)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
