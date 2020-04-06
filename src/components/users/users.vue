3213f9a372cfd279dc420aaea894a30fb0f8e169
yourcandy <545082571@qq.com>
2020-04-06 17:20:32 +0800

    用户列表 删除功能

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -10,18 +10,22 @@
     <el-row class="searchRow">
       <el-col>
         <el-input
+          @clear="loadUserList()"
+          clearable
           placeholder="请输入内容"
           v-model="query"
           class="inputSearch">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-button
+            @click="searchUser()"
+            slot="append" icon="el-icon-search"></el-button>
         </el-input>
-        <el-button type="success">添加用户</el-button>
+        <el-button @click="showAddUserDia()" type="success">添加用户</el-button>
       </el-col>
     </el-row>
 
     <!--表格-->
     <el-table
-      :data="tableData"
+      :data="userlist"
       style="width: 100%">
       <el-table-column
         type="index"
@@ -29,28 +33,81 @@
         width="60">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="username"
         label="姓名"
         width="80">
       </el-table-column>
       <el-table-column
-        prop="address"
+        prop="email"
         label="邮箱">
       </el-table-column>
       <el-table-column
-        prop="address"
+        prop="mobile"
         label="电话">
       </el-table-column>
       <el-table-column
-        prop="address"
         label="创建时间">
+        <template slot-scope="userlist">
+          {{userlist.row.create_time | fmtdate}}
+        </template>
       </el-table-column>
       <el-table-column
-        prop="address"
+        prop="mg_state"
         label="用户状态">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+          <el-button @click="showDeleUserMsgBox(scope.row.id)" size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+        </template>
       </el-table-column>
     </el-table>
     <!--分页-->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2,4,6,8]"
+      :page-size="2"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+
+    <!--对话框-->
+    <!--添加用户的对话框-->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名" label-width="100px">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" label-width="100px">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </el-card>
 </template>
 
@@ -59,24 +116,91 @@
     data() {
       return {
         query: '',
+        //分页相关数据
+        total: -1,
         pagenum: 1,
         pagesize: 2,
         //表格绑定的数据
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }]
+        userlist: [],
+        //添加对话框的是否可见属性
+        dialogFormVisibleAdd: false,
+        //添加用户的表单数据
+        form:{
+          username:'',
+          password:'',
+          email:'',
+          mobile:''
+        }
       }
     },
     created() {
-      this.getUserList()
+      this.getUserList();
     },
     methods: {
+      //删除用户 - 打开消息盒子
+      showDeleUserMsgBox(userId){
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async () => {
+          const res = await this.$http .delete(`users/${userId}`);
+          console.log(res);
+          if (res.data.meta.status === 200) {
+            //更新列表
+            this.pagenum = 1;
+            this.getUserList();
+            this.$message({
+              type: 'success',
+              message: res.data.meta.msg
+            });
+          }
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+      //添加用户 - 发送请求
+        async addUser(){
+        const res = await this.$http.post(`users`,this.form);
+        console.log(res);
+        const {meta:{status,msg},data} = res.data;
+        if (status === 201) {
+          this.dialogFormVisibleAdd = false;
+          this.$message.success(msg);
+          this.getUserList();
+          //清空文本框
+          this.form = {};
+        }else {
+          this.$message.warning(msg);
+        }
+      },
+      //添加用户 - 显示对话框
+      showAddUserDia(){
+        this.dialogFormVisibleAdd = true;
+      },
+      //清空搜索框 重新获取数据
+      loadUserList(){
+        this.getUserList();
+      },
+      //搜索用户
+      searchUser(){
+        console.log('按照搜索框中的query值去搜索用户');
+        this.getUserList();
+      },
+      //分页相关方法
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+        this.pagesize = val;
+        this.getUserList();
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+        this.pagenum = val;
+        this.getUserList();
+      },
       //获取用户列表的请求
       async getUserList() {
         //query  查询参数 可以为空
@@ -88,7 +212,18 @@
         const res = await this.$http.get(
           `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
         );
-        console.log(res)
+        console.log(res);
+        const {
+          meta: {status, msg},
+          data: {users, total}
+        } = res.data;
+        if (status === 200) {
+          this.userlist = users;
+          this.total = total;
+          this.$message.success(msg);
+        } else {
+          this.$message.warning(msg);
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
 import MyServerHttp from '@/plugins/http.js'
+import moment from 'moment'
 
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/reset.css';
@@ -11,7 +12,12 @@ import router from './router'
 Vue.use(MyServerHttp)
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//全局过滤器
+Vue.filter('fmtdate',(v)=>{
+  return moment(v).format('YYYY-MM-DD')
+});
 
 /* eslint-disable no-new */
 new Vue({
